7f1d9a2ff57f4e350ad9ba254ee38dad51173891
chionyan <ariariarissa@gmail.com>
2018-06-20 14:47:11 +0900

    [modify]welcome_system_spec.rb の it の説明文を追加

--- spec/system/welcome_system_spec.rb
+++ spec/system/welcome_system_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'WelcomeSystem', type: :system do
-  it '' do
+  it 'トップページにアクセスできること' do
     visit '/'
     expect(page).to have_content 'Welcome#index'
   end

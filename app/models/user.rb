897891d6382603cb2d58439561793b28056afcd7
chionyan <ariariarissa@gmail.com>
2018-06-28 15:01:29 +0900

    [clean]User.find_or_create_from_auth_hash 内の レシーバになっていた User を除去

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     nickname = auth_hash[:info][:nickname]
     image_url = auth_hash[:info][:image]
 
-    User.find_or_create_by(provider: provider, uid: uid) do |user|
+    find_or_create_by(provider: provider, uid: uid) do |user|
       user.nickname = nickname
       user.image_url = image_url
     end

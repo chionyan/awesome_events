7415a3df117997b235b1b13c3eece9925ef6f9ae
chionyan <ariariarissa@gmail.com>
2018-06-28 17:05:29 +0900

    [clean]sequence を n を使用した記法に修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     provider 'twitter'
-    sequence(:uid, 12_345, &:to_s)
+    sequence(:uid, 12345) { |n| "#{n}" }
     nickname 'netwillnet'
     image_url 'http://example.com/netwillnet.jpg'
   end

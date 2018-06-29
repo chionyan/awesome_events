db82bc21b871be955cf230ee6d1de26a6af821cb
chionyan <ariariarissa@gmail.com>
2018-06-29 18:43:26 +0900

    [feature]ログアウトの System Spec を実装

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -15,18 +15,49 @@ RSpec.describe 'SessionsSystem', type: :system do
 
   let(:user) { build(:user) }
 
-  context '"Twitterログイン"をクリックした時' do
+  context 'ログアウト時' do
+    before { visit root_path }
+
+    it '"Twitterでログイン"リンクが表示されること' do
+      expect(page).to have_content 'Twitterでログイン'
+    end
+
+    context '"Twitterログイン"をクリックした時' do
+      before do
+        visit root_path
+        click_link 'Twitterでログイン'
+      end
+
+      it 'トップページに遷移していること' do
+        expect(page.current_path).to eq root_path
+      end
+
+      it 'ログインしましたと表示されること' do
+        expect(page).to have_content 'ログインしました'
+      end
+    end
+  end
+
+  context 'ログイン時' do
     before do
       visit root_path
       click_link 'Twitterでログイン'
     end
 
-    it 'トップページに遷移していること' do
-      expect(page.current_path).to eq root_path
+    it '"ログアウト"リンクが表示されること' do
+      expect(page).to have_content 'ログアウト'
     end
 
-    it 'ログインしましたと表示されること' do
-      expect(page).to have_content 'ログインしました'
+    context '"ログアウト"をクリックした時' do
+      before { click_link 'ログアウト' }
+
+      it 'トップページに遷移していること' do
+        expect(page.current_path).to eq root_path
+      end
+
+      it 'ログアウトしましたと表示されること' do
+        expect(page).to have_content 'ログアウトしました'
+      end
     end
   end
 end

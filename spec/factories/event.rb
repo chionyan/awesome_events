ae328355ff69ceface161b9d418cb3b79b545f63
chionyan <ariariarissa@gmail.com>
2018-07-04 12:51:08 +0900

    [modify]Event Factory を修正
    - start_time, end_time に格納する値を文字列に変更
    - owner_id を sequence から固定値に変更

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :event do
-    sequence(:owner_id, 1)
+    owner_id 1
     name 'TEST_EVENT_NAME'
     place 'TEST_EVENT_PLACE'
     content 'TEST_EVENT_CONTENT'
-    start_time '2018-07-07 19:00:00'.in_time_zone
-    end_time '2018-07-07 21:00:00'.in_time_zone
+    start_time '2018-07-07 19:00:00'
+    end_time '2018-07-07 21:00:00'
   end
 end

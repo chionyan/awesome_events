d16059b5ad736f2d8753cc9002ca2fabd21f22be
chionyan <ariariarissa@gmail.com>
2018-07-06 14:51:13 +0900

    [feature]Event#create 実装
    - Request Spec, System Spec を作成
    - spec ファイルにおける遅延評価でない let を let! に変更

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Events', type: :request do
 
   describe 'GET #new' do
     subject { get new_event_path }
-    let(:user) { build(:user) }
+    let!(:user) { build(:user) }
 
     context 'ログイン時' do
       before do
@@ -34,6 +34,55 @@ RSpec.describe 'Events', type: :request do
 
       it 'トップページにリダイレクトすること' do
         expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    subject { post events_path, params: params }
+    let!(:user) { build(:user) }
+    let!(:event) { build(:event) }
+
+    before { get '/auth/twitter/callback' }
+
+    context '有効なパラメータの場合' do
+      let(:params) { { event: attributes_for(:event) } }
+
+      it 'HTTP Status 3xx が返ってくること' do
+        subject
+        expect(response).to be_redirect
+      end
+
+      it 'イベントを新規作成すること' do
+        expect { subject }.to change(Event, :count).by(1)
+      end
+
+      it ':show にリダイレクトすること' do
+        subject
+        expect(response).to redirect_to(Event.last)
+      end
+    end
+
+    context '無効なパラメータの場合' do
+      let(:params) { { event: attributes_for(:event, name: nil) } }
+
+      it 'HTTP Status 2xx が返ってくること' do
+        subject
+        expect(response).to be_successful
+      end
+
+      it 'イベントが作成されないこと' do
+        expect { subject }.not_to change(Event, :count)
+      end
+
+      it ':newテンプレートを再表示すること' do
+        subject
+        expect(response).to render_template :new
+      end
+
+      it 'エラーメッセージを表示すること' do
+        subject
+        expect(response.body).to include '<div class="alert alert-danger">'
       end
     end
   end

9e5bffdf84edd9fc42c78b5234505aecc96921e4
chionyan <ariariarissa@gmail.com>
2018-07-06 17:24:30 +0900

    [modify]#select の from の取る引数までの空白を半角スペース1文字分に修正
    - 引数の位置を揃えると、仮に新しいカラムが追加された場合、適宜調整が必要となるため

--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -30,16 +30,16 @@ RSpec.describe 'EventsSystem', type: :system do
         fill_in '名前', with: event.name
         fill_in '場所', with: event.place
         fill_in '内容', with: event.content
-        select event.start_time.year.to_s,    from: 'event_start_time_1i'
-        select event.start_time.month.to_s,   from: 'event_start_time_2i'
-        select event.start_time.day.to_s,     from: 'event_start_time_3i'
-        select event.start_time.hour.to_s.rjust(2, '0'),  from: 'event_start_time_4i'
-        select event.start_time.min.to_s.rjust(2, '0'),   from: 'event_start_time_5i'
-        select event.end_time.year.to_s,  from: 'event_end_time_1i'
+        select event.start_time.year.to_s, from: 'event_start_time_1i'
+        select event.start_time.month.to_s, from: 'event_start_time_2i'
+        select event.start_time.day.to_s, from: 'event_start_time_3i'
+        select event.start_time.hour.to_s.rjust(2, '0'), from: 'event_start_time_4i'
+        select event.start_time.min.to_s.rjust(2, '0'), from: 'event_start_time_5i'
+        select event.end_time.year.to_s, from: 'event_end_time_1i'
         select event.end_time.month.to_s, from: 'event_end_time_2i'
-        select event.end_time.day.to_s,   from: 'event_end_time_3i'
-        select event.end_time.hour.to_s.rjust(2, '0'),  from: 'event_end_time_4i'
-        select event.end_time.min.to_s.rjust(2, '0'),   from: 'event_end_time_5i'
+        select event.end_time.day.to_s, from: 'event_end_time_3i'
+        select event.end_time.hour.to_s.rjust(2, '0'), from: 'event_end_time_4i'
+        select event.end_time.min.to_s.rjust(2, '0'), from: 'event_end_time_5i'
       end
 
       it 'イベント詳細ページが表示されること' do
@@ -85,11 +85,11 @@ RSpec.describe 'EventsSystem', type: :system do
 
       context '開始時間が終了時間以上の場合' do
         before do
-          select event.end_time.year.to_s,    from: 'event_start_time_1i'
-          select event.end_time.month.to_s,   from: 'event_start_time_2i'
-          select event.end_time.day.to_s,     from: 'event_start_time_3i'
-          select event.end_time.hour.to_s.rjust(2, '0'),  from: 'event_start_time_4i'
-          select event.end_time.min.to_s.rjust(2, '0'),   from: 'event_start_time_5i'
+          select event.end_time.year.to_s, from: 'event_start_time_1i'
+          select event.end_time.month.to_s, from: 'event_start_time_2i'
+          select event.end_time.day.to_s, from: 'event_start_time_3i'
+          select event.end_time.hour.to_s.rjust(2, '0'), from: 'event_start_time_4i'
+          select event.end_time.min.to_s.rjust(2, '0'), from: 'event_start_time_5i'
         end
 
         it '"開始時間は終了時間よりも前に設定してください"メッセージが表示されること' do

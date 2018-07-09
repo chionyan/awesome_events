69b2ba046d318e2b2aa34a6a736461a2b7df68c0
chionyan <ariariarissa@gmail.com>
2018-07-09 17:06:21 +0900

    [ref]spec/system/sessions_spec.rb のリファクタリング
    - 前提条件毎に context ブロックを分ける
      - ユーザが未登録、登録済の場合
      - ユーザがログインしていない時、している時
    - 各条件下で before ブロックでくくれるものはまとめる
    - パス名、文言の修正

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -1,52 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe 'SessionsSystem', type: :system do
-  before { OmniAuth.config.mock_auth[:twitter] = log_in_as user }
+  before do
+    OmniAuth.config.mock_auth[:twitter] = log_in_as user
+    visit root_path
+  end
 
-  let(:user) { build(:user) }
+  context 'ユーザが未登録の場合' do
+    let(:user) { build(:user) }
 
-  context '未ログイン時' do
-    before { visit root_path }
+    context 'ユーザがログインしていない時' do
+      it '"Twitterでログイン"リンクが表示されること' do
+        expect(page).to have_content 'Twitterでログイン'
+      end
 
-    it '"Twitterでログイン"リンクが表示されること' do
-      expect(page).to have_content 'Twitterでログイン'
+      context '"Twitterログイン"をクリックした時' do
+        before { click_link 'Twitterでログイン' }
+
+        it 'トップページが表示されること' do
+          expect(page.current_path).to eq root_path
+        end
+
+        it 'ログインしましたと表示されること' do
+          expect(page).to have_content 'ログインしました'
+        end
+      end
     end
 
-    context '"Twitterログイン"をクリックした時' do
-      before do
-        visit root_path
-        click_link 'Twitterでログイン'
+    context 'ユーザがログインしている時' do
+      before { click_link 'Twitterでログイン' }
+
+      it '"ログアウト"リンクが表示されること' do
+        expect(page).to have_content 'ログアウト'
       end
 
-      it 'トップページに遷移していること' do
-        expect(page.current_path).to eq root_path
-      end
+      context '"ログアウト"をクリックした時' do
+        before { click_link 'ログアウト' }
 
-      it 'ログインしましたと表示されること' do
-        expect(page).to have_content 'ログインしました'
+        it 'トップページが表示されること' do
+          expect(page.current_path).to eq root_path
+        end
+
+        it 'ログアウトしましたと表示されること' do
+          expect(page).to have_content 'ログアウトしました'
+        end
       end
     end
   end
 
-  context 'ログイン時' do
-    before do
-      visit root_path
-      click_link 'Twitterでログイン'
-    end
+  context 'ユーザが登録済の場合' do
+    let(:user) { create(:user) }
 
-    it '"ログアウト"リンクが表示されること' do
-      expect(page).to have_content 'ログアウト'
-    end
-
-    context '"ログアウト"をクリックした時' do
-      before { click_link 'ログアウト' }
-
-      it 'トップページに遷移していること' do
-        expect(page.current_path).to eq root_path
+    context 'ユーザがログインしていない時' do
+      it '"Twitterでログイン"リンクが表示されること' do
+        expect(page).to have_content 'Twitterでログイン'
       end
 
-      it 'ログアウトしましたと表示されること' do
-        expect(page).to have_content 'ログアウトしました'
+      context '"Twitterログイン"をクリックした時' do
+        before { click_link 'Twitterでログイン' }
+
+        it 'トップページが表示されること' do
+          expect(page.current_path).to eq root_path
+        end
+
+        it 'ログインしましたと表示されること' do
+          expect(page).to have_content 'ログインしました'
+        end
+      end
+    end
+
+    context 'ユーザがログインしている時' do
+      before { click_link 'Twitterでログイン' }
+
+      it '"ログアウト"リンクが表示されること' do
+        expect(page).to have_content 'ログアウト'
+      end
+
+      context '"ログアウト"をクリックした時' do
+        before { click_link 'ログアウト' }
+
+        it 'トップページが表示されること' do
+          expect(page.current_path).to eq root_path
+        end
+
+        it 'ログアウトしましたと表示されること' do
+          expect(page).to have_content 'ログアウトしました'
+        end
       end
     end
   end

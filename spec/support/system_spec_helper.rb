e01ee96da18cd8756263e062c3f6b9535b44c0b0
chionyan <ariariarissa@gmail.com>
2018-07-13 18:00:46 +0900

    [feature]Event#edit, Event#update の System Spec 作成
    - System Spec の テストグループ構造を大幅に修正
    - System Spec におけるログイン処理を切り出し
    - #event_form_as の削除

--- spec/support/system_spec_helper.rb
+++ spec/support/system_spec_helper.rb
@@ -1,12 +1,4 @@
 module SystemSpecHelper
-  def event_form_as(event)
-    fill_in_text 'event_name', event.name
-    fill_in_text 'event_place', event.place
-    fill_in_text 'event_content', event.content
-    select_datetime 'event_start_time', event.start_time
-    select_datetime 'event_end_time', event.end_time
-  end
-
   def fill_in_text(id_name, content)
     fill_in id_name, with: content
   end
@@ -17,5 +9,15 @@ module SystemSpecHelper
     select datetime.day.to_s, from: "#{id_name}_3i"
     select datetime.hour.to_s.rjust(2, '0'), from: "#{id_name}_4i"
     select datetime.min.to_s.rjust(2, '0'), from: "#{id_name}_5i"
+  end
+
+  RSpec.shared_context 'with_log_in' do
+    let!(:user) { create(:user) }
+
+    before do
+      OmniAuth.config.mock_auth[:twitter] = log_in_as user
+      visit '/'
+      click_link 'Twitterでログイン'
+    end
   end
 end
--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -1,168 +1,128 @@
 require 'rails_helper'
 
 RSpec.describe 'EventsSystem', type: :system do
-  before do
-    OmniAuth.config.mock_auth[:twitter] = log_in_as user
-    travel_to '2018-07-07 18:30:00'
-    visit root_path
+  include_context 'with_log_in'
+
+  let!(:ja_name) { I18n.t('activerecord.attributes.event.name') }
+  let!(:ja_place) { I18n.t('activerecord.attributes.event.place') }
+  let!(:ja_content) { I18n.t('activerecord.attributes.event.content') }
+  let!(:ja_error_messages_blank) { I18n.t('errors.messages.blank') }
+
+  before { travel_to '2018-07-07 18:30:00' }
+
+  describe 'イベント作成ページ' do
+    before { click_link 'イベントを作る' }
+
+    it 'イベント作成ページに遷移すること' do
+      expect(page.current_path).to eq '/events/new'
+    end
+
+    context '無効な値を入力後、"作成"ボタンをクリックした場合' do
+      before do
+        fill_in_text 'event_name', ''
+        fill_in_text 'event_place', ''
+        fill_in_text 'event_content', ''
+        click_button '作成'
+      end
+
+      it '適切なエラーメッセージが表示されること' do
+        expect(page).to have_content "#{ja_name}#{ja_error_messages_blank}"
+        expect(page).to have_content "#{ja_place}#{ja_error_messages_blank}"
+        expect(page).to have_content "#{ja_content}#{ja_error_messages_blank}"
+        expect(page).to have_content '開始時間は終了時間よりも前に設定してください'
+      end
+    end
+
+    context '有効な値を入力後、"作成"ボタンをクリックした場合' do
+      let!(:event) { build(:event) }
+
+      before do
+        fill_in_text 'event_name', event.name
+        fill_in_text 'event_place', event.place
+        fill_in_text 'event_content', event.content
+        select_datetime 'event_start_time', event.start_time
+        select_datetime 'event_end_time', event.end_time
+        click_button '作成'
+      end
+
+      it '作成されたイベントの詳細ページに遷移すること' do
+        last_event = Event.order(:created_at).last
+
+        expect(page.current_path).to eq "/events/#{last_event.id}"
+        expect(page).to have_content last_event.name
+        expect(page).to have_content last_event.place
+        expect(page).to have_content last_event.content
+        expect(page).to have_content "#{last_event.start_time.strftime('%Y/%m/%d %H:%M')} - #{last_event.end_time.strftime('%Y/%m/%d %H:%M')}"
+      end
+
+      it '"作成しました" メッセージが表示されていること' do
+        expect(page).to have_content '作成しました'
+      end
+
+      it '"イベントを編集する" ボタンが表示されていること' do
+        expect(page).to have_content 'イベントを編集する'
+      end
+    end
   end
 
-  context 'ユーザがログインしていない時' do
-    let(:user) { create(:user) }
+  describe 'イベント編集ページ' do
+    let!(:event) { create(:event, owner: user) }
 
-    context 'イベント作成ページにアクセスした時' do
-      let(:event) { build(:event) }
-      before { visit new_event_path }
-
-      it 'トップページにリダイレクトされること' do
-        expect(page.current_path).to eq root_path
-      end
-      it 'ログインしてくださいと表示されること' do
-        expect(page).to have_content 'ログインしてください'
-      end
+    before do
+      click_link 'AwesomeEvents'
+      click_link event.name
+      click_link 'イベントを編集する'
     end
 
-    context 'イベント詳細ページにアクセスした時' do
-      let!(:event) { create(:event) }
-      before { visit event_path(event.id) }
-
-      it 'イベントの詳細が表示されること' do
-        expect(page).to have_event_attribute(event)
-      end
+    it 'イベント編集ページに遷移すること' do
+      expect(page.current_path).to eq "/events/#{event.id}/edit"
     end
 
-    context 'イベント一覧ページにアクセスした場合' do
-      let!(:event) { create(:event) }
-      let!(:past_event) { create(:past_event) }
-      let!(:future_event) { create(:future_event) }
-      before { visit events_path }
-
-      it '未開催のイベント一覧が表示されること' do
-        expect(page).to have_content event.name, future_event.name
-        expect(page).to_not have_content past_event.name
+    describe '名前を編集後、"更新"ボタンをクリックした時' do
+      before do
+        fill_in_text 'event_name', "#{event.name}_edit"
+        click_button '更新'
       end
 
-      it '開始時間順でイベント一覧が表示されること' do
-        list_group_item = page.all('.list-group-item')
-
-        expect(list_group_item[0].find('h4').text).to eq event.name
-        expect(list_group_item[1].find('h4').text).to eq future_event.name
+      it '更新されたイベントの詳細ページに遷移すること' do
+        expect(page.current_path).to eq "/events/#{event.id}"
+        expect(page).to have_content "#{event.name}_edit"
       end
 
-      context 'イベント名リンクをクリックした時' do
-        before { click_link event.name }
-
-        it 'イベントの詳細ページに遷移すること' do
-          expect(page.current_path).to eq event_path(event.id)
-        end
+      it '"更新しました"メッセージが表示されていること' do
+        expect(page).to have_content '更新しました'
       end
     end
   end
 
-  context 'ユーザがログインしている時' do
-    before { click_link 'Twitterでログイン' }
+  describe '未ログイン時のイベント一覧ページ' do
+    let!(:event) { create(:event, owner: user) }
+    let!(:past_event) { create(:past_event) }
+    let!(:future_event) { create(:future_event) }
 
-    let(:user) { create(:user) }
+    before { click_link 'ログアウト' }
 
-    context 'イベント作成ページにアクセスした時' do
-      before { visit new_event_path }
+    it '開始時間順に未開催のイベント一覧が表示されること' do
+      list_group_item = page.all('.list-group-item')
 
-      context 'フォームに有効なパラメータを入力後、"作成"ボタンをクリックした場合' do
-        let(:event) { build(:event) }
-        before do
-          event_form_as event
-          click_button '作成'
-        end
-
-        it 'イベント詳細ページにリダイレクトされること' do
-          expect(page.current_path).to eq event_path(Event.order(:created_at).last)
-        end
-        it 'イベントの詳細が表示されること' do
-          expect(page).to have_event_attribute(event)
-        end
-        it '"作成しました"メッセージが表示されること' do
-          expect(page).to have_content '作成しました'
-        end
-      end
-
-      context 'フォームに無効なパラメータを入力後、"作成"ボタンをクリックした場合' do
-        let(:event) { build(:event, params) }
-        let(:ja_attribute) { I18n.t("activerecord.attributes.event.#{params.keys.first}") }
-        let!(:ja_error_messages_blank) { I18n.t('errors.messages.blank') }
-
-        before do
-          event_form_as event
-          click_button '作成'
-        end
-
-        context '名前が空の場合' do
-          let(:params) { { name: '' } }
-
-          it '適切なエラーメッセージが表示されること' do
-            expect(page).to have_content "#{ja_attribute}#{ja_error_messages_blank}"
-          end
-        end
-
-        context '場所が空の場合' do
-          let(:params) { { place: '' } }
-
-          it '適切なエラーメッセージが表示されること' do
-            expect(page).to have_content "#{ja_attribute}#{ja_error_messages_blank}"
-          end
-        end
-
-        context '内容が空の場合' do
-          let(:params) { { content: '' } }
-
-          it '適切なエラーメッセージが表示されること' do
-            expect(page).to have_content "#{ja_attribute}#{ja_error_messages_blank}"
-          end
-        end
-
-        context '開始時間が終了時間以上の場合' do
-          let(:params) { { start_time: '2018-07-07 22:00:00' } }
-
-          it '適切なエラーメッセージが表示されること' do
-            expect(page).to have_content '開始時間は終了時間よりも前に設定してください'
-          end
-        end
-      end
+      expect(list_group_item[0].find('h4').text).to eq event.name
+      expect(list_group_item[1].find('h4').text).to eq future_event.name
+      expect(page).to_not have_content past_event.name
     end
 
-    context 'イベント詳細ページにアクセスした時' do
-      let!(:event) { create(:event) }
-      before { visit event_path(event.id) }
+    describe 'イベント名のリンクをクリックした時' do
+      before { click_link event.name }
 
-      it 'イベントの詳細が表示されること' do
-        expect(page).to have_event_attribute(event)
-      end
-    end
-
-    context 'イベント一覧ページにアクセスした場合' do
-      let!(:event) { create(:event) }
-      let!(:past_event) { create(:past_event) }
-      let!(:future_event) { create(:future_event) }
-
-      before { visit events_path }
-
-      it '未開催のイベント一覧が表示されること' do
-        expect(page).to have_content event.name, future_event.name
-        expect(page).to_not have_content past_event.name
+      it 'イベントの詳細ページに遷移すること' do
+        expect(page.current_path).to eq "/events/#{event.id}"
+        expect(page).to have_content event.name
+        expect(page).to have_content event.place
+        expect(page).to have_content event.content
+        expect(page).to have_content "#{event.start_time.strftime('%Y/%m/%d %H:%M')} - #{event.end_time.strftime('%Y/%m/%d %H:%M')}"
       end
 
-      it '開始時間順でイベント一覧が表示されること' do
-        list_group_item = page.all('.list-group-item')
-
-        expect(list_group_item[0].find('h4').text).to eq event.name
-        expect(list_group_item[1].find('h4').text).to eq future_event.name
-      end
-
-      context 'イベント名リンクをクリックした時' do
-        before { click_link event.name }
-
-        it 'イベントの詳細ページに遷移すること' do
-          expect(page.current_path).to eq event_path(event.id)
-        end
+      it '"イベントを編集する"リンクが表示されないこと' do
+        expect(page).to_not have_content 'イベントを編集する'
       end
     end
   end

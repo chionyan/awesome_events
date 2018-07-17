dc1fc88377df29d8d12f35b2dac86daf8d6becfd
chionyan <ariariarissa@gmail.com>
2018-07-17 11:23:00 +0900

    [modify]Event factory を trait を使用した記法に修正

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -6,23 +6,22 @@ FactoryBot.define do
     content 'TEST_EVENT_CONTENT'
     start_time '2018-07-07 19:00:00'
     end_time '2018-07-07 22:00:00'
-  end
 
-  factory :past_event, class: Event do
-    owner
-    name 'past_event_name'
-    place 'past_event_place'
-    content 'past_event_content'
-    start_time '2018-07-07 18:00:00'
-    end_time '2018-07-07 22:00:00'
-  end
+    trait :past_event_attr do
+      name 'past_event_name'
+      place 'past_event_place'
+      content 'past_event_content'
+      start_time '2018-07-07 18:00:00'
+    end
 
-  factory :future_event, class: Event do
-    owner
-    name 'future_event_name'
-    place 'future_event_place'
-    content 'future_event_content'
-    start_time '2018-07-07 20:00:00'
-    end_time '2018-07-07 22:00:00'
+    trait :future_event_attr do
+      name 'future_event_name'
+      place 'future_event_place'
+      content 'future_event_content'
+      start_time '2018-07-07 20:00:00'
+    end
+
+    factory :past_event, traits: [:past_event_attr]
+    factory :future_event, traits: [:future_event_attr]
   end
 end

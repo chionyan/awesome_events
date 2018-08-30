7350b41fd830d0c8bf262e958ea9285aa1aad0b3
chionyan <ariariarissa@gmail.com>
2018-08-30 16:01:59 +0900

    [feature] 退会したユーザの表示をする

--- spec/system/retire_spec.rb
+++ spec/system/retire_spec.rb
@@ -63,4 +63,42 @@ RSpec.describe 'RetireSystem', type: :system do
       end
     end
   end
+
+  describe 'ユーザが退会した時' do
+    subject do
+      travel_to '2018-07-07 17:00:00'
+      click_link 'AwesomeEvents'
+      click_link past_event.name
+    end
+
+    before do
+      travel_to '2018-07-07 23:00:00'
+      click_link '退会'
+    end
+
+    context '主催者が退会した場合' do
+      let!(:past_event) { create(:event, :past_event, owner: user) }
+
+      before { click_link '退会する' }
+
+      it '主催者に"退会したユーザです"が表示されること' do
+        subject
+        organizer = page.find(:css, 'div:nth-child(1) > div.card-body')
+        expect(organizer.text).to eq '退会したユーザです'
+      end
+    end
+
+    context '参加者が退会した場合' do
+      let!(:past_event) { create(:event, :past_event, owner: other_user) }
+      let!(:ticket) { create(:ticket, user: user, event: past_event) }
+
+      before { click_link '退会する' }
+
+      it '参加者に"退会したユーザです"が表示されること' do
+        subject
+        list_group = page.find(:css, 'div.card-body > ul')
+        expect(list_group.text).to have_content '退会したユーザです'
+      end
+    end
+  end
 end
